d7bc94d5a034490aa62503e57fe106898489755b
Madishettygayathri <madishettygayathri94@gmail.com>
2024-11-17 13:37:13 +0530

    initial

---
+++ prime.java
@@ -0,0 +1,18 @@
+class prime{
+    public static void main(string[]args)
+    {
+        int n=23,flag=0;
+        for(int i=2;i<=Math.sqrt(n);i++)
+        {
+            if(n%i==0)
+            {
+                flag=1;
+                break;
+            }
+        }
+        if(flag==0)
+        System.out.println("yes");
+        else
+         System.out.println("no");
+    }
+}
